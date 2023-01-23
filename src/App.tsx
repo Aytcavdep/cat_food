7db745f0a20b5061a3745ea38c54aaab49776261
Коваленко Владимир <sazonov2380@gmail.com>
2023-01-24 01:50:14 +0300

    added logic: hover, selected, disabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './components/Card';
 import { catFoodItem } from './components/catFoodItem';
 import './scss/App.scss';
 
+export type HoverType = number[];
+export type SelectedType = number[];
+export type DisabledType = number[];
+
+export type UseStatePropsType = {
+  isHover: (id: number) => boolean;
+  setIsHover: (id: number) => void;
+  isSelected: (id: number) => boolean;
+  setIsSelected: (id: number) => void;
+  isDisabled: (id: number) => boolean;
+  setIsDisabled: (id: number) => void;
+};
+
 function App() {
+  const [hover, setHover] = useState([0]);
+  const [selected, setSelected] = useState([0]);
+  const [disabled, setDisabled] = useState([0]);
+
+  const isHover = (id: number) => {
+    return hover.includes(id);
+  };
+  const isSelected = (id: number) => {
+    return selected.includes(id);
+  };
+  const isDisabled = (id: number) => {
+    return disabled.includes(id);
+  };
+
+  const setIsHover = (id: number) => {
+    if (hover.includes(id)) {
+      return setHover((prev) => [...prev].filter((item) => item !== id));
+    }
+    setHover((prev) => [...prev, id]);
+  };
+  const setIsSelected = (id: number) => {
+    if (selected.includes(id)) {
+      return setSelected((prev) => [...prev].filter((item) => item !== id));
+    }
+    setSelected((prev) => [...prev, id]);
+  };
+  const setIsDisabled = (id: number) => {
+    if (disabled.includes(id)) {
+      return setDisabled((prev) => [...prev].filter((item) => item !== id));
+    }
+    setDisabled((prev) => [...prev, id]);
+  };
+  const useStateProps = {
+    isHover,
+    setIsHover,
+    isSelected,
+    setIsSelected,
+    isDisabled,
+    setIsDisabled,
+  };
   return (
     <div className="App">
+      {/* Кнопки для проверки функционала Disabled */}
+      <div className="button_disabled_area">
+        <button onClick={() => setIsDisabled(1)}>disabled 1</button>
+        <button onClick={() => setIsDisabled(2)}>disabled 2</button>
+        <button onClick={() => setIsDisabled(3)}>disabled 3</button>
+      </div>
       <div className="header">Ты сегодня покормил кота?</div>
       <div className="card_area">
         {catFoodItem.map((item) => {
-          return <Card key={item.id} {...item} />;
+          return <Card key={item.id} {...item} {...useStateProps} />;
         })}
       </div>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import '../scss/components/Card.scss';
-import cat from '../assets/img/Photo.png';
 import { CatFoodItemType } from '../components/catFoodItem';
+import { UseStatePropsType } from '../App';
 
-const Card: React.FC<CatFoodItemType> = ({
+//кастомный атрибут
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    // extends React's HTMLAttributes
+    disabled?: boolean;
+  }
+}
+
+interface CardPropsType extends CatFoodItemType, UseStatePropsType {}
+
+const Card: React.FC<CardPropsType> = ({
   id,
   title,
   titleSelectedHover,
@@ -16,23 +26,61 @@ const Card: React.FC<CatFoodItemType> = ({
   footer,
   footerSelected,
   footerDisabled,
+  isHover,
+  setIsHover,
+  isSelected,
+  setIsSelected,
+  isDisabled,
+  setIsDisabled,
 }) => {
   return (
     <div className="card_layout">
-      <div className="card_border">
-        <div className="card">
-          <div className="card_title">{title}</div>
-          <div className="card_brand_name">{brand}</div>
-          <div className="card_taste">{taste}</div>
-          <div className="card_offer">
+      <div
+        disabled={isDisabled(id)}
+        onMouseEnter={() => setIsHover(id)}
+        onMouseLeave={() => setIsHover(id)}
+        onClick={() => setIsSelected(id)}
+        className={
+          isSelected(id) ? 'card_border card_border_selected' : 'card_border'
+        }
+      >
+        <div className={isHover(id) ? 'card card_hover' : 'card'}>
+          <div
+            disabled={isDisabled(id)}
+            className={
+              isHover(id) && isSelected(id)
+                ? 'card_title card_title_selected_hover'
+                : 'card_title'
+            }
+          >
+            {isHover(id) && isSelected(id) ? titleSelectedHover : title}
+          </div>
+          <div disabled={isDisabled(id)} className="card_brand_name">
+            {brand}
+          </div>
+          <div disabled={isDisabled(id)} className="card_taste">
+            {taste}
+          </div>
+          <div disabled={isDisabled(id)} className="card_offer">
             <span>{offer.count}</span> {offer.serving}
             <br />
             <span>{offer.giftCount}</span> {offer.gift}
           </div>
           <div className="card_label_weight">
             <div className="card_label">
-              <img alt="Foto cat" src={img} />
-              <div className="card_weight">
+              <img disabled={isDisabled(id)} alt="Foto cat" src={img} />
+              <div
+                disabled={isDisabled(id)}
+                className={
+                  isHover(id) && isSelected(id)
+                    ? 'card_weight weight_selected_hover'
+                    : isSelected(id)
+                    ? 'card_weight weight_selected'
+                    : isHover(id)
+                    ? 'card_weight weight_hover'
+                    : 'card_weight'
+                }
+              >
                 <div className="weight">{foodWeight}</div>
                 <div className="unit">{foodUnit}</div>
               </div>
@@ -40,9 +88,19 @@ const Card: React.FC<CatFoodItemType> = ({
           </div>
         </div>
       </div>
-      <div className="card_footer">
-        {footer}
-        <span className="link">купи</span>.
+      <div disabled={isDisabled(id)} className="card_footer">
+        {isDisabled(id)
+          ? footerDisabled
+          : !isSelected(id)
+          ? footer
+          : footerSelected}
+        {!isSelected(id) && !isDisabled(id) ? (
+          <span onClick={() => setIsSelected(id)} className="link">
+            купи.
+          </span>
+        ) : (
+          ''
+        )}
       </div>
     </div>
   );
